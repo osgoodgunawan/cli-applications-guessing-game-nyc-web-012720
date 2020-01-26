ac51c888b2f569f8b7323994e4d3cf44fc0d4c67
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-26 05:27:17 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
   
   input=gets.chomp
   
-  if input== random_num.to_s
+  if input== random_num
     puts "You guessed the correct number!"
   else
     puts "Sorry! The computer guessed #{random_num}."
